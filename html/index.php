91412dcaa36de588c77a94269c211f81a28dd72d
tyamcha <david99111@mail.ru>
2022-03-04 11:25:39 +0300

    final_added_structure_of_frontend

--- html/index.php
+++ html/index.php
@@ -4,10 +4,12 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+	<link rel="stylesheet" href="./style/style.css">
+	<title>Docker PHP template</title>
 </head>
 <body>
 	Hello world!
-	<?php include('script.php') ?>
+	<?php include('backend/script.php') ?>
+	<script src="./script/script.js"></script>
 </body>
 </html>
